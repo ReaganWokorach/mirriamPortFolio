26058a372a84d57450d944ca9917d88bf5512f36
Reagan Wokorach <106845223+ReaganWokorach@users.noreply.github.com>
2026-05-05 17:29:15 +0300

    Third version update

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 const hamburger = document.getElementById('hamburger');
 const navMenu = document.getElementById('nav-menu');
+const icon = hamburger.querySelector('i');
 
+// Toggle Mobile Menu
 hamburger.addEventListener('click', () => {
-  navMenu.classList.toggle('active');
+    navMenu.classList.toggle('active');
+    icon.classList.toggle('fa-bars');
+    icon.classList.toggle('fa-times');
 });
 
+// Close Menu on Link Click & Smooth Scroll
 document.querySelectorAll('.nav-links a').forEach(link => {
-  link.addEventListener('click', () => {
-    navMenu.classList.remove('active');
-  });
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        
+        window.scrollTo({
+            top: target.offsetTop - 90,
+            behavior: 'smooth'
+        });
+
+        navMenu.classList.remove('active');
+        icon.classList.add('fa-bars');
+        icon.classList.remove('fa-times');
+    });
 });
