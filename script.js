ad1af9afdfa17733952bd2328ff5f345eccf150d
Reagan Wokorach <106845223+ReaganWokorach@users.noreply.github.com>
2026-05-05 17:19:26 +0300

    Including all the info in resume

--- script.js
+++ script.js
@@ -1,31 +1,12 @@
 const hamburger = document.getElementById('hamburger');
 const navMenu = document.getElementById('nav-menu');
-const icon = hamburger.querySelector('i');
 
-// Toggle Mobile Menu
 hamburger.addEventListener('click', () => {
-    navMenu.classList.toggle('active');
-    icon.classList.toggle('fa-bars');
-    icon.classList.toggle('fa-times');
+  navMenu.classList.toggle('active');
 });
 
-// Close menu on link click
 document.querySelectorAll('.nav-links a').forEach(link => {
-    link.addEventListener('click', () => {
-        navMenu.classList.remove('active');
-        icon.classList.add('fa-bars');
-        icon.classList.remove('fa-times');
-    });
-});
-
-// Smooth Scroll Offset
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        const target = document.querySelector(this.getAttribute('href'));
-        window.scrollTo({
-            top: target.offsetTop - 90,
-            behavior: 'smooth'
-        });
-    });
+  link.addEventListener('click', () => {
+    navMenu.classList.remove('active');
+  });
 });
